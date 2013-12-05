0f8f84b0bca27335bb8f62b74a98944997928005
James Hughes <jhughes@sci.utah.edu>
2013-12-05 14:50:08 -0700

    Initial commit of AABB code.

---
+++ glm-aabb/AABB.cpp
@@ -0,0 +1,204 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2013 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+/// \author James Hughes
+/// \date   December 2013
+
+#include "aabb.hpp"
+#include <glm/gtx/component_wise.hpp>
+
+AABB::AABB()
+{
+  setNull();
+}
+
+AABB::AABB(const glm::vec3& center, glm::float_t radius)
+{
+  extend(center, radius);
+}
+
+AABB::AABB(const glm::vec3& p1, const glm::vec3& p2)
+{
+  extend(p1);
+  extend(p2);
+}
+
+AABB::~AABB()
+{
+}
+
+void AABB::extend(glm::float_t val)
+{
+  if (!isNull())
+  {
+    mMin -= glm::vec3(val);
+    mMax += glm::vec3(val);
+  }
+}
+
+void AABB::extend(const glm::vec3& p)
+{
+  if (!isNull())
+  {
+    mMin = glm::min(p, mMin);
+    mMax = glm::max(p, mMax);
+  }
+  else
+  {
+    mMin = p;
+    mMax = p;
+  }
+}
+
+void AABB::extend(const glm::vec3& p, glm::float_t r)
+{
+  glm::vec3 r(radius);
+  if (!isNull())
+  {
+    mMin = glm::min(p - r, mMin);
+    mMax = glm::max(p + r, mMax);
+  }
+  else
+  {
+    mMin = p - r;
+    mMax = p + r;
+  }
+}
+
+void AABB::extend(const AABB& aabb)
+{
+  if (aabb.valid())
+  {
+    extend(aabb.mMin);
+    extend(aabb.mMax);
+  }
+}
+
+void AABB::extendDisk(const glm::vec3& c, const glm::vec3& n, glm::float_t r)
+{
+  if (glm::length(n) < 1.e-12) { extend(c); return; }
+  n = glm::normalize(n);
+  glm::float_t x = sqrt(1 - n.x()) * r;
+  glm::float_t y = sqrt(1 - n.y()) * r;
+  glm::float_t z = sqrt(1 - n.z()) * r;
+  extend(cen + glm::vec3(x,y,z));
+  extend(cen - glm::vec3(x,y,z));
+}
+
+glm::vec3 AABB::getDiagonal() const
+{
+  if (!isNull())
+    return mMin - mMax;
+  else
+    return glm::vec3(0);
+}
+
+glm::float_t AABB::getLongestEdge() const
+{
+  return glm::compMax(getDiagonal());
+}
+
+glm::float_t AABB::getShortestEdge() const
+{
+  return glm::compMin(getDiagonal());
+}
+
+void AABB::translate(const glm::vec3& v)
+{
+  mMin += v;
+  mMax += v;
+}
+
+void AABB::scale(const glm::vec3& v)
+{
+  mMin -= o;
+  mMax -= o;
+
+  mMin *= s;
+  mMax *= s;
+
+  mMin += o;
+  mMax += o;
+}
+
+bool AABB::overlaps(const AABB& bb) const
+{
+  if (isNull() || bb.isNull())
+    return false;
+
+  if( bb.mMin.x() > mMax.x() || bb.mMax.x() < mMin.x())
+    return false;
+  else if( bb.mMin.y() > mMax.y() || bb.mMax.y() < mMin.y())
+    return false;
+  else if( bb.mMin.z() > mMax.z() || bb.mMax.z() < mMin.z())
+    return false;
+
+  return true;
+}
+
+AABB::INTERSECTION_TYPE intersect(const AABB& b) const
+{
+  if (isNull() || bb.isNull())
+    return OUTSIDE;
+
+  if ((mMax.x() < b.mMin.x()) || (mMin.x() > b.mMax.x()) ||
+      (mMax.y() < b.mMin.y()) || (mMin.y() > b.mMax.y()) ||
+      (mMax.z() < b.mMin.z()) || (mMin.z() > b.mMax.z())) 
+  {
+    return OUTSIDE;
+  }
+
+  if ((mMin.x() <= b.mMin.x()) && (mMax.x() >= b.mMax.x()) &&
+      (mMin.y() <= b.mMin.y()) && (mMax.y() >= b.mMax.y()) &&
+      (mMin.z() <= b.mMin.z()) && (mMax.z() >= b.mMax.z())) 
+  {
+    return INSIDE;
+  }
+
+  return INTERSECT;    
+}
+
+
+bool AABB::isSimilarTo(const AABB& b, glm::float_t diff = 0.5) const
+{
+  if (isNull() || b.isNull()) return false;
+
+  glm::vec3 acceptable_diff=((getDiagonal()+b.getDiagonal())/2.0)*diff;
+  glm::vec3 min_diff(mMin-b.mMin);
+  min_diff = glm::vec3(fabs(min_diff.x()),fabs(min_diff.y()),fabs(min_diff.z()));
+  if (min_diff.x()>acceptable_diff.x()) return false;
+  if (min_diff.y()>acceptable_diff.y()) return false;
+  if (min_diff.z()>acceptable_diff.z()) return false;
+  glm::vec3 max_diff(mMax-b.mMax);
+  max_diff = glm::vec3(fabs(max_diff.x()),fabs(max_diff.y()),fabs(max_diff.z()));
+  if (max_diff.x()>acceptable_diff.x()) return false;
+  if (max_diff.y()>acceptable_diff.y()) return false;
+  if (max_diff.z()>acceptable_diff.z()) return false;
+  return true;
+}
+
---
+++ glm-aabb/AABB.hpp
@@ -0,0 +1,124 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2013 James Hughes.
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+/// \author James Hughes
+/// \date   December 2013
+
+#ifndef IAUNS_GLM_AABB_HPP
+#define IAUNS_GLM_AABB_HPP
+
+#include <glm/glm.hpp>
+
+namespace CPM_GLM_AABB_NS {
+
+/// Axis aligned bounding box implemented on top of GLM.
+class AABB
+{
+public:
+  /// Builds a null AABB.
+  AABB();
+
+  /// Builds an AABB that encompasses a sphere of \p radius and \p center.
+  AABB(const glm::vec3& center, glm::float_t radius);
+
+  /// Builds an AABB that contains the two points.
+  AABB(const glm::vec3& p1, const glm::vec3& p2);
+
+  ~AABB();
+
+  /// Set the AABB as NULL (not set).
+  void setNull()      {mMin = glm::vec3(1.0); mMax = glm::vec3(1.0);}
+
+  /// Returns true if AABB is NULL (not set).
+  void isNull() const {return mMin.x > mMax.x || mMin.y > mMax.y || mMin.z > mMax.z;}
+
+  /// Extend the bounding box on all sides by \p val.
+  void extend(glm::float_t val);
+
+  /// Expand the AABB to include point \p p.
+  void extend(const glm::vec3& p);
+
+  /// Expand the AABB to include a sphere centered at \p p and of radius \p r.
+  void extend(const glm::vec3& p, glm::float_t r);
+
+  /// Expand the AABB to encompass the given AABB.
+  void extend(const AABB& aabb);
+
+  /// Expand the AABB to include a disk centered at \p c, with normal \p n,
+  /// and radius \p r.
+  void extendDisk(const glm::vec3& c, const glm::vec3& n, glm::float_t r);
+
+  /// Translates AABB by vector \p v.
+  void translate(const glm::vec3& v);
+
+  /// Scale the AABB by \p s, centered around \p o.
+  void scale(const glm::vec3& v);
+
+  /// Retrieves the diagonal vector (computed as mMin - mMax).
+  /// If the AABB is NULL, then a vector of all zeros is returned.
+  glm::vec3 getDiagonal() const;
+
+  /// Retrieves the longest edge.
+  /// If the AABB is NULL, then 0 is returned.
+  glm::float_t getLongestEdge() const;
+
+  /// Retrieves the shortest edge.
+  /// If the AABB is NULL, then 0 is returned.
+  glm::float_t getShortestEdge() const;
+
+  /// Retrieves the AABB's minimum point.
+  glm::vec3 getMin() const {return mMin;}
+
+  /// Retrieves the AABB's maximum point.
+  glm::vec3 getMax() const {return mMax;}
+
+  /// Returns true if AABBs share a face overlap.
+  bool overlaps(const AABB& bb) const;
+
+  enum INTERSECTION_TYPE { INSIDE, INTERSECT, OUTSIDE };
+  /// Returns one of the intersection types. If either of the aabbs are invalid,
+  /// then OUTSIDE is returned.
+  INTERSECTION_TYPE intersect(const AABB& bb) const;
+
+
+  /// Function from SCIRun. Here is a summary of SCIRun's description:
+  /// Returns true if the two AABB's are similar. If diff is 1.0, the two
+  /// bboxes have to have about 50% overlap each for x,y,z. If diff is 0.0,
+  /// they have to have 100% overlap.
+  /// If either of the two AABBs is NULL, then false is returned.
+  bool isSimilarTo(const AABB& b, glm::float_t diff = 0.5) const;
+
+
+protected:
+
+  glm::vec3 mMin;
+  glm::vec3 mMax;
+};
+
+} // namespace CPM_GLM_AABB_NS 
+
+#endif 
